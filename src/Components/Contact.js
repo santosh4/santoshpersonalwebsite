6b045c61c692e23fc2e3902939016d58ce3ebf6c
Santosh Mutyala <santosh.mutyala987@gmail.com>
2019-05-21 17:17:08 +0530

    first commit

---
+++ src/Components/Contact.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+
+class Contact extends Component {
+  render() {
+
+    if(this.props.data){
+      var name = this.props.data.name;
+      var street = this.props.data.address.street;
+      var city = this.props.data.address.city;
+      var state = this.props.data.address.state;
+      var zip = this.props.data.address.zip;
+      var phone= this.props.data.phone;
+      var email = this.props.data.email;
+      var message = this.props.data.contactmessage;
+    }
+
+    return (
+      <section id="contact">
+
+         <div className="row section-head">
+         <h1>Get In Touch</h1>
+            <div className="ten columns">
+                  <p className="lead">{message}</p>
+            </div>
+         </div>
+
+         <div className="row">
+					   <h4>Contact Details</h4>
+					   <p className="address">
+						   {name}<br />
+						   {street} <br />
+						   {city}, {state} {zip}<br />
+						   <span>{phone}</span>
+					   </p>
+      </div>
+   </section>
+    );
+  }
+}
+
+export default Contact;
